2127134c138088382b76e5a3c01bee47f948da4e
Steinar Bang <sb@dod.no>
2015-05-02 18:24:30 +0200

    Use the Jsr330Activator to expose a PropertysetManager service.
    Remove the existing Activator and its test, and embed the
    Jsr330Activator and javax.inject instead, and use the Jsr330Activator as
    the bundle's activator, and add a Provider<PropertysetManager> for the
    Jsr330Activator to find and use to expose a service.

---
+++ modelstore.implementation/src/main/java/no/priv/bang/modeling/modelstore/impl/PropertysetManagerProvider.java
@@ -0,0 +1,22 @@
+package no.priv.bang.modeling.modelstore.impl;
+
+import javax.inject.Provider;
+
+import no.priv.bang.modeling.modelstore.PropertysetManager;
+import no.steria.osgi.jsr330activator.Jsr330Activator;
+
+/**
+ * A thin wrapper around {@link DefaultPropertysetManager} that will
+ * be picked up by the {@link Jsr330Activator} and be presented
+ * in OSGi as a {@link PropertysetManager} service.
+ *
+ * @author Steinar Bang
+ *
+ */
+public class PropertysetManagerProvider implements Provider<PropertysetManager> {
+
+    public PropertysetManager get() {
+        return DefaultPropertysetManager.getInstance();
+    }
+
+}
