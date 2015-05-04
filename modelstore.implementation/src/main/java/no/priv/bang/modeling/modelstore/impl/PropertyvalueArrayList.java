a73b6ba8296129e3851c7ac6336d88b246f07c06
Steinar Bang <sb@dod.no>
2015-05-04 18:48:30 +0200

    Made PropertyvalueArrayList extend AbstractList<Propertyvalue>.
    This reduced the size of the implementation code.

--- modelstore.implementation/src/main/java/no/priv/bang/modeling/modelstore/impl/PropertyvalueArrayList.java
+++ modelstore.implementation/src/main/java/no/priv/bang/modeling/modelstore/impl/PropertyvalueArrayList.java
@@ -1,116 +1,38 @@
 package no.priv.bang.modeling.modelstore.impl;
 
+import java.util.AbstractList;
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Iterator;
 import java.util.List;
-import java.util.ListIterator;
-
 import no.priv.bang.modeling.modelstore.Propertyvalue;
 import no.priv.bang.modeling.modelstore.PropertyvalueList;
 
-public class PropertyvalueArrayList implements PropertyvalueList {
+public class PropertyvalueArrayList extends AbstractList<Propertyvalue> implements PropertyvalueList {
 
     List<Propertyvalue> arrayList = new ArrayList<Propertyvalue>();
 
-    public boolean add(Propertyvalue e) {
-        return arrayList.add(e);
+    @Override
+    public Propertyvalue set(int paramInt, Propertyvalue paramE) {
+        return arrayList.set(paramInt, paramE);
     }
 
-    public void add(int index, Propertyvalue element) {
-        arrayList.add(index, element);
+    @Override
+    public void add(int paramInt, Propertyvalue paramE) {
+        arrayList.add(paramInt, paramE);
     }
 
-    public boolean addAll(Collection<? extends Propertyvalue> c) {
-        return arrayList.addAll(c);
+    @Override
+    public Propertyvalue remove(int paramInt) {
+        return arrayList.remove(paramInt);
     }
 
-    public boolean addAll(int index, Collection<? extends Propertyvalue> c) {
-        return arrayList.addAll(index, c);
-    }
-
-    public void clear() {
-        arrayList.clear();
-    }
-
-    public boolean contains(Object o) {
-        return arrayList.contains(o);
-    }
-
-    public boolean containsAll(Collection<?> c) {
-        return arrayList.containsAll(c);
-    }
-
-    public boolean equals(Object o) {
-        return arrayList.equals(o);
-    }
-
+    @Override
     public Propertyvalue get(int index) {
         return arrayList.get(index);
     }
 
-    public int hashCode() {
-        return arrayList.hashCode();
-    }
-
-    public int indexOf(Object o) {
-        return arrayList.indexOf(o);
-    }
-
-    public boolean isEmpty() {
-        return arrayList.isEmpty();
-    }
-
-    public Iterator<Propertyvalue> iterator() {
-        return arrayList.iterator();
-    }
-
-    public int lastIndexOf(Object o) {
-        return arrayList.lastIndexOf(o);
-    }
-
-    public ListIterator<Propertyvalue> listIterator() {
-        return arrayList.listIterator();
-    }
-
-    public ListIterator<Propertyvalue> listIterator(int index) {
-        return arrayList.listIterator(index);
-    }
-
-    public boolean remove(Object o) {
-        return arrayList.remove(o);
-    }
-
-    public Propertyvalue remove(int index) {
-        return arrayList.remove(index);
-    }
-
-    public boolean removeAll(Collection<?> c) {
-        return arrayList.removeAll(c);
-    }
-
-    public boolean retainAll(Collection<?> c) {
-        return arrayList.retainAll(c);
-    }
-
-    public Propertyvalue set(int index, Propertyvalue element) {
-        return arrayList.set(index, element);
-    }
-
+    @Override
     public int size() {
         return arrayList.size();
-    }
-
-    public List<Propertyvalue> subList(int fromIndex, int toIndex) {
-        return arrayList.subList(fromIndex, toIndex);
-    }
-
-    public Object[] toArray() {
-        return arrayList.toArray();
-    }
-
-    public <T> T[] toArray(T[] a) {
-        return arrayList.toArray(a);
     }
 
 }
