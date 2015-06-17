7ee15859b27c6ebbb2eab27a1934e47bc67d7882
Steinar Bang <sb@dod.no>
2015-06-17 21:14:05 +0200

    Added unit tests for BooleanPropertyvalue.
    Simplified BooleanPropertyvalue.hashCode() and
    BooleanPropertyvalue.equals() since BooleanPropertyvalue.value will
    never be null.

--- modelstore.implementation/src/main/java/no/priv/bang/modeling/modelstore/impl/BooleanPropertyvalue.java
+++ modelstore.implementation/src/main/java/no/priv/bang/modeling/modelstore/impl/BooleanPropertyvalue.java
@@ -68,7 +68,7 @@ class BooleanPropertyvalue extends PropertyvalueBase {
     public int hashCode() {
         final int prime = 31;
         int result = 1;
-        result = prime * result + ((value == null) ? 0 : value.hashCode());
+        result = prime * result + value.hashCode();
         return result;
     }
 
@@ -87,15 +87,7 @@ class BooleanPropertyvalue extends PropertyvalueBase {
         }
 
         BooleanPropertyvalue other = (BooleanPropertyvalue) obj;
-        if (value == null) {
-            if (other.value != null) {
-                return false;
-            }
-        } else if (!value.equals(other.value)) {
-            return false;
-        }
-
-        return true;
+        return value.equals(other.value);
     }
 
     @Override
--- modelstore.implementation/src/test/java/no/priv/bang/modeling/modelstore/impl/BooleanPropertyvalueTest.java
+++ modelstore.implementation/src/test/java/no/priv/bang/modeling/modelstore/impl/BooleanPropertyvalueTest.java
@@ -105,4 +105,42 @@ public class BooleanPropertyvalueTest {
     	assertTrue(emptyList.isEmpty());
     }
 
+    /**
+     * Test av {@link BooleanPropertyvalue#hashCode()}.
+     */
+    @Test
+    public void testHashCode() {
+        BooleanPropertyvalue nullBooleanValue = new BooleanPropertyvalue(null);
+        assertEquals(1268, nullBooleanValue.hashCode());
+        BooleanPropertyvalue trueBooleanValue = new BooleanPropertyvalue(Boolean.TRUE);
+        assertEquals(1262, trueBooleanValue.hashCode());
+    }
+
+    /**
+     * Test av {@link BooleanPropertyvalue#equals(Object)}.
+     */
+    @Test
+    public void testEquals() {
+        BooleanPropertyvalue nullBooleanValue = new BooleanPropertyvalue(null);
+        BooleanPropertyvalue falseBooleanValue = new BooleanPropertyvalue(Boolean.FALSE);
+        BooleanPropertyvalue trueBooleanValue = new BooleanPropertyvalue(Boolean.TRUE);
+        assertFalse(nullBooleanValue.equals(null));
+        assertFalse(nullBooleanValue.equals(PropertyvalueNil.getNil()));
+        assertTrue(nullBooleanValue.equals(nullBooleanValue));
+        assertTrue(nullBooleanValue.equals(falseBooleanValue));
+        assertFalse(nullBooleanValue.equals(trueBooleanValue));
+    }
+
+    /**
+     * Test av {@link BooleanPropertyvalue#toString()}.
+     */
+    @Test
+    public void testToString() {
+        BooleanPropertyvalue nullBooleanValue = new BooleanPropertyvalue(null);
+        assertEquals("BooleanPropertyvalue [value=false]", nullBooleanValue.toString());
+        BooleanPropertyvalue falseBooleanValue = new BooleanPropertyvalue(Boolean.FALSE);
+        assertEquals("BooleanPropertyvalue [value=false]", falseBooleanValue.toString());
+        BooleanPropertyvalue trueBooleanValue = new BooleanPropertyvalue(Boolean.TRUE);
+        assertEquals("BooleanPropertyvalue [value=true]", trueBooleanValue.toString());
+    }
 }
