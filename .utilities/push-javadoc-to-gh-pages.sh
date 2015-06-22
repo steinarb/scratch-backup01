2a61911e8402972769855a3dfaa94b973486f353
Steinar Bang <sb@dod.no>
2015-06-22 21:18:23 +0200

    Fixed what's hopefully the last copy/paste error.

--- .utilities/push-javadoc-to-gh-pages.sh
+++ .utilities/push-javadoc-to-gh-pages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ "$TRAVIS_REPO_SLUG" == "sbang/jsr330activator" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
+if [ "$TRAVIS_REPO_SLUG" == "steinarb/modelstore" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
 
   echo -e "Publishing javadoc...\n"
 
