d9520220de6629b6e2b24470e62f1a96b018bb90
Steinar Bang <sb@dod.no>
2018-09-26 19:09:53 +0200

    fixup! Add notifications about wage payments

--- ukelonn.web.frontend/src/main/frontend/components/App.js
+++ ukelonn.web.frontend/src/main/frontend/components/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect, Provider } from 'react-redux';
 import { Switch, Route, BrowserRouter as Router, NavLink } from 'react-router-dom';
-import { NotificationSystem } from 'react-notification-system';
+import NotificationSystem from 'react-notification-system';
 import Home from "./Home";
 import Login from "./Login";
 import User from "./User";
@@ -34,27 +34,30 @@ class App extends Component {
     render() {
         return(
             <Provider store={this.state.store}>
-                <Router>
-                    <Switch>
-                        <Route exact path="/ukelonn/" component={Home} />
-                        <Route path="/ukelonn/login*" component={Login} />
-                        <Route path="/ukelonn/user" component={User} />
-                        <Route path="/ukelonn/performedjobs" component={PerformedJobs} />
-                        <Route path="/ukelonn/performedpayments" component={PerformedPayments} />
-                        <Route path="/ukelonn/admin/jobtypes/modify" component={AdminJobtypesModify} />
-                        <Route path="/ukelonn/admin/jobtypes/create" component={AdminJobtypesCreate} />
-                        <Route path="/ukelonn/admin/jobtypes" component={AdminJobtypes} />
-                        <Route path="/ukelonn/admin/jobs/delete" component={AdminJobsDelete} />
-                        <Route path="/ukelonn/admin/paymenttypes/modify" component={AdminPaymenttypesModify} />
-                        <Route path="/ukelonn/admin/paymenttypes/create" component={AdminPaymenttypesCreate} />
-                        <Route path="/ukelonn/admin/paymenttypes" component={AdminPaymenttypes} />
-                        <Route path="/ukelonn/admin/users/password" component={AdminUsersChangePassword} />
-                        <Route path="/ukelonn/admin/users/modify" component={AdminUsersModify} />
-                        <Route path="/ukelonn/admin/users/create" component={AdminUsersCreate} />
-                        <Route path="/ukelonn/admin/users" component={AdminUsers} />
-                        <Route path="/ukelonn/admin" component={Admin} />
-                    </Switch>
-                </Router>
+                <div>
+                    <NotificationSystem/>
+                    <Router>
+                        <Switch>
+                            <Route exact path="/ukelonn/" component={Home} />
+                            <Route path="/ukelonn/login*" component={Login} />
+                            <Route path="/ukelonn/user" component={User} />
+                            <Route path="/ukelonn/performedjobs" component={PerformedJobs} />
+                            <Route path="/ukelonn/performedpayments" component={PerformedPayments} />
+                            <Route path="/ukelonn/admin/jobtypes/modify" component={AdminJobtypesModify} />
+                            <Route path="/ukelonn/admin/jobtypes/create" component={AdminJobtypesCreate} />
+                            <Route path="/ukelonn/admin/jobtypes" component={AdminJobtypes} />
+                            <Route path="/ukelonn/admin/jobs/delete" component={AdminJobsDelete} />
+                            <Route path="/ukelonn/admin/paymenttypes/modify" component={AdminPaymenttypesModify} />
+                            <Route path="/ukelonn/admin/paymenttypes/create" component={AdminPaymenttypesCreate} />
+                            <Route path="/ukelonn/admin/paymenttypes" component={AdminPaymenttypes} />
+                            <Route path="/ukelonn/admin/users/password" component={AdminUsersChangePassword} />
+                            <Route path="/ukelonn/admin/users/modify" component={AdminUsersModify} />
+                            <Route path="/ukelonn/admin/users/create" component={AdminUsersCreate} />
+                            <Route path="/ukelonn/admin/users" component={AdminUsers} />
+                            <Route path="/ukelonn/admin" component={Admin} />
+                        </Switch>
+                    </Router>
+                </div>
             </Provider>
         );
     }
