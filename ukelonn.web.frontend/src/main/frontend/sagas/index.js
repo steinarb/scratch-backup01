965f3434ec46717a27014fb68c19e57c73d67b2a
Steinar Bang <sb@dod.no>
2018-09-29 12:03:53 +0200

    fixup! Try browser desktop notifications

--- ukelonn.web.frontend/src/main/frontend/sagas/index.js
+++ ukelonn.web.frontend/src/main/frontend/sagas/index.js
@@ -244,11 +244,20 @@ function doRegisterPayment(payment) {
     return axios.post('/ukelonn/api/registerpayment', payment);
 }
 
+function doNotifyPaymentdone(payment, paymenttype) {
+    const notification = {
+        title: 'Ukelønn',
+        message: payment.transactionAmount + ' kroner ' + paymenttype.transactionTypeName,
+    };
+    return axios.post('/ukelonn/api/notificationto/' + payment.account.username, notification);
+}
+
 // worker saga
 function* receiveRegisterPaymentSaga(action) {
     try {
         const response = yield call(doRegisterPayment, action.payment);
         const account = (response.headers['content-type'] == 'application/json') ? response.data : emptyAccount;
+        doNotifyPaymentdone(action.payment, action.paymenttype);
         yield put({ type: 'REGISTERPAYMENT_RECEIVE', account: account });
     } catch (error) {
         yield put({ type: 'REGISTERPAYMENT_FAILURE', error });
