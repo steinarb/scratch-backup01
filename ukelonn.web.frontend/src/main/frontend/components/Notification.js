df611f8da1168318ac4f3efbe1026a17c5d9af7d
Steinar Bang <sb@dod.no>
2018-09-29 22:17:15 +0200

    fixup! Try browser desktop notifications

--- ukelonn.web.frontend/src/main/frontend/components/Notification.js
+++ ukelonn.web.frontend/src/main/frontend/components/Notification.js
@@ -1,10 +1,12 @@
 import { spawnNotification } from "./spawnnotification.js";
 import React from 'react';
+import { connect } from 'react-redux';
 
-const Notification = ({notificationMessage}) => {
+var Notification = ({notificationMessage, onNullNotification}) => {
     if (notificationMessage) {
         if (Notification) {
             spawnNotification(notificationMessage);
+            onNullNotification();
         } else {
             console.log("Notification not supported by browser");
         }
@@ -12,5 +14,19 @@ const Notification = ({notificationMessage}) => {
 
     return null;
 };
+
+const mapStateToProps = state => {
+    return {
+        notificationMessage: state.notificationMessage,
+    };
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+        onNullNotification: () => dispatch({ type: 'UPDATE', data: { notificationMessage: null } }),
+    };
+};
+
+Notification = connect(mapStateToProps, mapDispatchToProps)(Notification);
 
 export default Notification;
