9748fcd8f6d8b09ee83b738c3d9814e85b69127a
Steinar Bang <sb@dod.no>
2018-10-11 20:04:26 +0200

    Remove local notification because it caused the application to fail on iOS

--- ukelonn.web.frontend/src/main/frontend/index.js
+++ ukelonn.web.frontend/src/main/frontend/index.js
@@ -13,20 +13,6 @@ const history = createBrowserHistory();
 const store = createStore(ukelonnReducer, compose(applyMiddleware(sagaMiddleware, routerMiddleware(history)), window.devToolsExtension ? window.devToolsExtension() : f => f));
 sagaMiddleware.run(rootSaga);
 
-console.log('Her(1)');
-
-
-if (Notification) {
-    console.log('Her(2)');
-    Notification.requestPermission().then(function(result) {
-        store.dispatch({ type: 'UPDATE', data: { notificationAvailable: true } });
-        console.log(result);
-    });
-}
-
-console.log('Her(3)');
-
-
 ReactDOM.render(
     <App store={store} />,
     document.getElementById('root')
