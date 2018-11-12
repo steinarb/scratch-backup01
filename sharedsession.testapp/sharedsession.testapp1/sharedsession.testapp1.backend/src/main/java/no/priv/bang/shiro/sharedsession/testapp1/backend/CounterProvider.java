61e4fcb0d3b3caf4536d0a083ae53b94eac59b70
Steinar Bang <sb@dod.no>
2018-11-12 15:57:47 +0100

    fixup! Add test applications

---
+++ sharedsession.testapp/sharedsession.testapp1/sharedsession.testapp1.backend/src/main/java/no/priv/bang/shiro/sharedsession/testapp1/backend/CounterProvider.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2018 Steinar Bang
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package no.priv.bang.shiro.sharedsession.testapp1.backend;
+
+import org.osgi.service.component.annotations.Component;
+
+import no.priv.bang.shiro.sharedsession.testapp1.services.Counter;
+
+@Component(service=Counter.class, immediate=true)
+public class CounterProvider implements Counter {
+    private long counter;
+
+    public long current() {
+        return counter;
+    }
+
+    public long increment() {
+        return ++counter;
+    }
+
+}
