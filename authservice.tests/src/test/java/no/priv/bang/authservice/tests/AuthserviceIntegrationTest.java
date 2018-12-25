6241831ee6514e892247d3cb354c17e4d8e87946
Steinar Bang <sb@dod.no>
2018-12-25 09:01:28 +0100

    fixup! Use DS instead of Jsr330Activator and move to modular design

--- authservice.tests/src/test/java/no/priv/bang/authservice/tests/AuthserviceIntegrationTest.java
+++ authservice.tests/src/test/java/no/priv/bang/authservice/tests/AuthserviceIntegrationTest.java
@@ -41,6 +41,7 @@ public class AuthserviceIntegrationTest {
             karafDistributionConfiguration().frameworkUrl(karafUrl).unpackDirectory(new File("target/exam")).useDeployFolder(false).runEmbedded(true),
             configureConsole().ignoreLocalConsole().ignoreRemoteShell(),
             systemTimeout(60000),
+            keepRuntimeFolder(),
             logLevel(LogLevel.DEBUG),
             editConfigurationFilePut("etc/org.apache.karaf.management.cfg", "rmiRegistryPort", RMI_REG_PORT),
             editConfigurationFilePut("etc/org.apache.karaf.management.cfg", "rmiServerPort", RMI_SERVER_PORT),
