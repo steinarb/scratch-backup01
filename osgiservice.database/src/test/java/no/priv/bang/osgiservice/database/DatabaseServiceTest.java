2c38cda35ea92eaebaefea1fb0b472cc847c6c94
Steinar Bang <sb@dod.no>
2019-01-04 19:00:03 +0100

    Make groupId/artifactId more maven-bundle-plugin friendly by changing "osgi.service" to "osgiservice"

--- osgiservice.database/src/test/java/no/priv/bang/osgiservice/database/DatabaseServiceTest.java
+++ osgiservice.database/src/test/java/no/priv/bang/osgiservice/database/DatabaseServiceTest.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and limitations
  * under the License.
  */
-package no.priv.bang.osgi.service.database;
+package no.priv.bang.osgiservice.database;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
