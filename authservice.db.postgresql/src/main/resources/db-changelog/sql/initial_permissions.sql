39cdb5a18f20b0ba2f3a358282dcd7e10e11fb94
Steinar Bang <sb@dod.no>
2019-03-20 20:03:36 +0100

    fixup! Add a PostgreSQL database for authservice

--- authservice.db.postgresql/src/main/resources/db-changelog/sql/initial_permissions.sql
+++ authservice.db.postgresql/src/main/resources/db-changelog/sql/initial_permissions.sql
@@ -1,8 +1,3 @@
 --liquibase formatted sql
---changeset sb:initial_permissions
-insert into permissions (permission_name, description) values ('user_admin_api_read','User admin read access');
-insert into permissions (permission_name, description) values ('user_admin_api_write','User admin write access');
-insert into permissions (permission_name, description) values ('caseworker_read','Caseworker read access');
-insert into permissions (permission_name, description) values ('caseworker_write','Caseworker write access');
-insert into permissions (permission_name, description) values ('user_read','User read access');
+--changeset sb:empty_initial_permissions
 --rollback delete from permissions; alter table permissions alter permission_id restart with 1;
