fbb82bab2cc49c5681e223e1408a41c36397cdbe
Steinar Bang <sb@dod.no>
2019-05-17 08:25:42 +0200

    Add a PostgreSQL database for handlereg

---
+++ handlereg.db.postgresql/src/main/java/no/priv/bang/handlereg/db/postgresql/HandleregPosgresqlDatabase.java
@@ -0,0 +1,129 @@
+/*
+ * Copyright 2019 Steinar Bang
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package no.priv.bang.handlereg.db.postgresql;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Map;
+import java.util.Properties;
+
+import javax.sql.DataSource;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.jdbc.DataSourceFactory;
+import org.osgi.service.log.LogService;
+
+import liquibase.Liquibase;
+import liquibase.database.DatabaseConnection;
+import liquibase.database.jvm.JdbcConnection;
+import liquibase.exception.LiquibaseException;
+import liquibase.resource.ClassLoaderResourceAccessor;
+import no.priv.bang.handlereg.db.liquibase.HandleregLiquibase;
+import no.priv.bang.handlereg.services.HandleregDatabase;
+import no.priv.bang.handlereg.services.HandleregException;
+import static no.priv.bang.handlereg.services.HandleregConstants.*;
+
+@Component(service=HandleregDatabase.class, immediate=true)
+public class HandleregPosgresqlDatabase implements HandleregDatabase {
+
+    private LogService logservice;
+    private DataSourceFactory dataSourceFactory;
+    DataSource dataSource;
+
+    @Reference
+    public void setLogService(LogService logservice) {
+        this.logservice = logservice;
+    }
+
+    @Reference
+    public void setDataSourceFactory(DataSourceFactory dataSourceFactory) {
+        this.dataSourceFactory = dataSourceFactory;
+    }
+
+    @Activate
+    public void activate(Map<String, Object> config) {
+        try {
+            dataSource = createDatasource(config);
+            try (Connection connect = getConnection()) {
+                HandleregLiquibase handleregLiquibase = new HandleregLiquibase();
+                handleregLiquibase.createInitialSchema(connect);
+                insertMockData(connect);
+                handleregLiquibase.updateSchema(connect);
+            }
+        } catch (Exception e) {
+            String message = "Failed to create handlereg derby test database";
+            logError(message, e);
+            throw new HandleregException(message, e);
+        }
+    }
+
+    @Override
+    public Connection getConnection() throws SQLException {
+        return dataSource.getConnection();
+    }
+
+    @Override
+    public boolean forceReleaseLiquibaseLock() {
+        try (Connection connection = getConnection()) {
+            HandleregLiquibase liquibase = new HandleregLiquibase();
+            liquibase.forceReleaseLocks(connection);
+            return true;
+        } catch (Exception e) {
+            logError("Failed to force release Liquibase changelog lock on database", e);
+            return false;
+        }
+    }
+
+    @Override
+    public DataSource getDatasource() {
+        return dataSource;
+    }
+
+    public void insertMockData(Connection connect) throws LiquibaseException, SQLException {
+        DatabaseConnection databaseConnection = new JdbcConnection(connect);
+        ClassLoaderResourceAccessor classLoaderResourceAccessor = new ClassLoaderResourceAccessor(getClass().getClassLoader());
+        Liquibase liquibase = new Liquibase("sql/data/db-changelog.xml", classLoaderResourceAccessor, databaseConnection);
+        liquibase.update("");
+    }
+
+    private void logError(String message, Exception exception) {
+        logservice.log(LogService.LOG_ERROR, message, exception);
+    }
+
+
+    private DataSource createDatasource(Map<String, Object> config) throws SQLException {
+        Properties properties = createDatabaseConnectionProperties(config);
+        return dataSourceFactory.createDataSource(properties);
+    }
+
+    static Properties createDatabaseConnectionProperties(Map<String, Object> config) {
+        String jdbcUrl = ((String) config.getOrDefault(HANDLEREG_JDBC_URL, "jdbc:postgresql:///handlereg")).trim();
+        String jdbcUser = ((String) config.getOrDefault(HANDLEREG_JDBC_USER, "karaf")).trim();
+        String jdbcPassword = ((String) config.getOrDefault(HANDLEREG_JDBC_PASSWORD, "karaf")).trim();
+        Properties properties = new Properties();
+        properties.setProperty(DataSourceFactory.JDBC_URL, jdbcUrl);
+        if (!"".equals(jdbcUser)) {
+            properties.setProperty(DataSourceFactory.JDBC_USER, jdbcUser);
+        }
+        if (!"".equals(jdbcPassword)) {
+            properties.setProperty(DataSourceFactory.JDBC_PASSWORD, jdbcPassword);
+        }
+
+        return properties;
+    }
+
+}
---
+++ handlereg.db.postgresql/src/test/java/no/priv/bang/handlereg/db/postgresql/HandleregPostgresqlDatabaseTest.java
@@ -0,0 +1,197 @@
+/*
+ * Copyright 2019 Steinar Bang
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package no.priv.bang.handlereg.db.postgresql;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.sql.DataSource;
+import org.junit.jupiter.api.Test;
+import org.ops4j.pax.jdbc.derby.impl.DerbyDataSourceFactory;
+import org.osgi.service.jdbc.DataSourceFactory;
+
+import no.priv.bang.handlereg.services.HandleregException;
+import no.priv.bang.osgi.service.mocks.logservice.MockLogService;
+import static no.priv.bang.handlereg.services.HandleregConstants.*;
+
+class HandleregPostgresqlDatabaseTest {
+
+    @Test
+    void testCreateAndVerifySomeDataInSomeTables() throws Exception {
+        DataSourceFactory dataSourceFactory = new DerbyDataSourceFactory();
+        MockLogService logservice = new MockLogService();
+        HandleregPosgresqlDatabase database = new HandleregPosgresqlDatabase();
+        database.setLogService(logservice);
+        database.setDataSourceFactory(dataSourceFactory);
+        Map<String, Object> config = new HashMap<>();
+        config.put(HANDLEREG_JDBC_URL, "jdbc:derby:memory:ukelonn;create=true");
+        database.activate(config);
+        int jdId = addAccount(database, "jd");
+        assertAccounts(database);
+        int storeid = addStore(database, "Spar Næroset");
+        int originalNumberOfTransactions = findNumberOfTransactions(database);
+        addTransaction(database, jdId, storeid, 138);
+        int updatedNumberOfTransactions = findNumberOfTransactions(database);
+        assertEquals(originalNumberOfTransactions + 1, updatedNumberOfTransactions);
+
+        // Connection brukes av Shiro JdbcRealm
+        DataSource datasource = database.getDatasource();
+        assertEquals("Apache Derby", datasource.getConnection().getMetaData().getDatabaseProductName());
+    }
+
+    @SuppressWarnings("unchecked")
+    @Test
+    void testFailDuringActivation() {
+        assertThrows(HandleregException.class, () -> {
+                DataSourceFactory dataSourceFactory = mock(DataSourceFactory.class);
+                when(dataSourceFactory.createConnectionPoolDataSource(any())).thenThrow(SQLException.class);
+                MockLogService logservice = new MockLogService();
+                HandleregPosgresqlDatabase database = new HandleregPosgresqlDatabase();
+                database.setLogService(logservice);
+                database.setDataSourceFactory(dataSourceFactory);
+                Map<String, Object> config = null;
+                database.activate(config);
+                fail("Should never get here");
+            });
+    }
+
+    @Test
+    public void testForceReleaseLocks() {
+        DataSourceFactory dataSourceFactory = new DerbyDataSourceFactory();
+        MockLogService logservice = new MockLogService();
+        HandleregPosgresqlDatabase database = new HandleregPosgresqlDatabase();
+        database.setLogService(logservice);
+        database.setDataSourceFactory(dataSourceFactory);
+        Map<String, Object> config = new HashMap<>();
+        config.put(HANDLEREG_JDBC_URL, "jdbc:derby:memory:ukelonn;create=true");
+        database.activate(config);
+
+        boolean result = database.forceReleaseLiquibaseLock();
+        assertTrue(result);
+    }
+
+    @Test
+    public void testForceReleaseLocksFailing() throws Exception {
+        // Need a real database to create the object
+        DataSourceFactory dataSourceFactory = new DerbyDataSourceFactory();
+        MockLogService logservice = new MockLogService();
+        HandleregPosgresqlDatabase database = new HandleregPosgresqlDatabase();
+        database.setLogService(logservice);
+        database.setDataSourceFactory(dataSourceFactory);
+        Map<String, Object> config = new HashMap<>();
+        config.put(HANDLEREG_JDBC_URL, "jdbc:derby:memory:ukelonn;create=true");
+        database.activate(config);
+
+        // Replace the mock datasource returning a connection that will fail when
+        // releasing the lock
+        Connection connection = mock(Connection.class);
+        DataSource datasource = mock(DataSource.class);
+        when(datasource.getConnection()).thenReturn(connection);
+        database.dataSource = datasource;
+
+        boolean result = database.forceReleaseLiquibaseLock();
+        assertFalse(result);
+    }
+
+    private void assertAccounts(HandleregPosgresqlDatabase database) throws Exception {
+        try (Connection connection = database.getConnection()) {
+            try(PreparedStatement statement = connection.prepareStatement("select * from accounts")) {
+                try (ResultSet results = statement.executeQuery()) {
+                    assertAccount(results, "jd");
+                }
+            }
+        }
+    }
+
+    private void assertAccount(ResultSet results, String username) throws Exception {
+        assertTrue(results.next());
+        assertEquals(username, results.getString(2)); // column 1 is the id
+    }
+
+    private int addAccount(HandleregPosgresqlDatabase database, String username) throws Exception {
+        try (Connection connection = database.getConnection()) {
+            try(PreparedStatement statement = connection.prepareStatement("insert into accounts (username) values (?)")) {
+                statement.setString(1, username);
+                statement.executeUpdate();
+            }
+        }
+        int accountId = -1;
+        try (Connection connection = database.getConnection()) {
+            try(PreparedStatement statement = connection.prepareStatement("select * from accounts where username=?")) {
+                statement.setString(1, username);
+                try (ResultSet results = statement.executeQuery()) {
+                    results.next();
+                    accountId = results.getInt(1);
+                }
+            }
+        }
+        return accountId;
+    }
+
+    private int addStore(HandleregPosgresqlDatabase database, String storename) throws Exception {
+        try (Connection connection = database.getConnection()) {
+            try(PreparedStatement statement = connection.prepareStatement("insert into stores (store_name) values (?)")) {
+                statement.setString(1, storename);
+                statement.executeUpdate();
+            }
+        }
+        int storeid = -1;
+        try (Connection connection = database.getConnection()) {
+            try(PreparedStatement statement = connection.prepareStatement("select * from stores where store_name=?")) {
+                statement.setString(1, storename);
+                try (ResultSet results = statement.executeQuery()) {
+                    results.next();
+                    storeid = results.getInt(1);
+                }
+            }
+        }
+        return storeid;
+    }
+
+    private void addTransaction(HandleregPosgresqlDatabase database, int accountid, int storeid, double amount) throws SQLException {
+        try (Connection connection = database.getConnection()) {
+            try(PreparedStatement statement = connection.prepareStatement("insert into transactions (account_id, store_id, transaction_amount) values (?, ?, ?)")) {
+                statement.setInt(1, accountid);
+                statement.setInt(2, storeid);
+                statement.setDouble(3, amount);
+                statement.executeUpdate();
+            }
+        }
+    }
+
+    private int findNumberOfTransactions(HandleregPosgresqlDatabase database) throws SQLException {
+        try (Connection connection = database.getConnection()) {
+            try(PreparedStatement statement = connection.prepareStatement("select * from transactions")) {
+                try (ResultSet results = statement.executeQuery()) {
+                    int count = 0;
+                    while(results.next()) {
+                        ++count;
+                    }
+
+                    return count;
+                }
+            }
+        }
+    }
+
+}
