6fe8ded8999112a7d38856e5865521ff19cc124b
Steinar Bang <sb@dod.no>
2019-07-15 22:16:14 +0200

    fixup! Add statistics functions sum earnings over year and over month

--- ukelonn.web.frontend/src/main/frontend/sagas/earningsSumOverMonthSaga.js
+++ ukelonn.web.frontend/src/main/frontend/sagas/earningsSumOverMonthSaga.js
@@ -1,9 +1,10 @@
-import { takeLatest, call, put, fork } from "redux-saga/effects";
+import { all, takeLatest, call, put, fork } from "redux-saga/effects";
 import axios from "axios";
 import {
     EARNINGS_SUM_OVER_MONTH_REQUEST,
     EARNINGS_SUM_OVER_MONTH_RECEIVE,
     EARNINGS_SUM_OVER_MONTH_FAILURE,
+    REGISTERJOB_RECEIVE,
 } from '../actiontypes';
 
 function doEarningsSumOverMonth(username) {
@@ -24,5 +25,7 @@ function* receiveEarningsSumOverMonthSaga(action) {
 
 // watcher saga
 export default function* earningsSumOverMonthSaga() {
-    yield takeLatest(EARNINGS_SUM_OVER_MONTH_REQUEST, receiveEarningsSumOverMonthSaga);
+    yield all([
+        yield takeLatest(EARNINGS_SUM_OVER_MONTH_REQUEST, receiveEarningsSumOverMonthSaga),
+    ]);
 }
--- ukelonn.web.frontend/src/main/frontend/sagas/earningsSumOverYearSaga.js
+++ ukelonn.web.frontend/src/main/frontend/sagas/earningsSumOverYearSaga.js
@@ -1,9 +1,10 @@
-import { takeLatest, call, put, fork } from "redux-saga/effects";
+import { all, takeLatest, call, put, fork } from "redux-saga/effects";
 import axios from "axios";
 import {
     EARNINGS_SUM_OVER_YEAR_REQUEST,
     EARNINGS_SUM_OVER_YEAR_RECEIVE,
     EARNINGS_SUM_OVER_YEAR_FAILURE,
+    REGISTERJOB_RECEIVE,
 } from '../actiontypes';
 
 function doEarningsSumOverYear(username) {
@@ -24,5 +25,7 @@ function* receiveEarningsSumOverYearSaga(action) {
 
 // watcher saga
 export default function* earningsSumOverYearSaga() {
-    yield takeLatest(EARNINGS_SUM_OVER_YEAR_REQUEST, receiveEarningsSumOverYearSaga);
+    yield all([
+        takeLatest(EARNINGS_SUM_OVER_YEAR_REQUEST, receiveEarningsSumOverYearSaga),
+    ]);
 }
