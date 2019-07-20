8a8db2c8d0a6e1275be24b757e1946bd2805295a
Steinar Bang <sb@dod.no>
2019-07-20 22:40:20 +0200

    fixup! Add statistics functions sum earnings over year and over month

--- ukelonn.web.frontend/src/main/frontend/components/StatisticsEarningsSumOverYear.js
+++ ukelonn.web.frontend/src/main/frontend/components/StatisticsEarningsSumOverYear.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import { Redirect } from 'react-router';
 import { Link } from 'react-router-dom';
 import { findUsernameFromAccountOrQueryParameter } from '../common/account';
+import { userIsLoggedIn } from '../common/login';
 import {
     ACCOUNT_REQUEST,
     LOGOUT_REQUEST,
@@ -15,17 +16,34 @@ class StatisticsEarningsSumOverYear extends Component {
     }
 
     render() {
-        let { haveReceivedResponseFromLogin, loginResponse, onLogout } = this.props;
-
-        if (haveReceivedResponseFromLogin && loginResponse.roles.length === 0) {
+        if (userIsLoggedIn(this.props)) {
             return <Redirect to="/ukelonn/login" />;
         }
+
+        let { earningsSumOverYear, onLogout } = this.props;
+
 
         return (
             <div>
                 <h1>Sum av lønn pr år</h1>
                 <br/>
                 <Link to="/ukelonn/statistics">Tilbake til statistikk</Link><br/>
+                <table className="table table-bordered">
+                    <thead>
+                        <tr>
+                            <td>År</td>
+                            <td>Totalt tjent</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {earningsSumOverYear.map((sumOverYear) =>
+                            <tr key={sumOverYear.year}>
+                                <td>{sumOverYear.year}</td>
+                                <td>{sumOverYear.sum}</td>
+                            </tr>
+                        )}
+                    </tbody>
+                </table>
                 <br/>
                 <button onClick={() => onLogout()}>Logout</button>
                 <br/>
