42fa5a4ef4233bdc1326a3209eb8367df88ef8e4
Steinar Bang <sb@dod.no>
2019-12-16 23:12:15 +0100

    Make react components stateless

--- handlereg.web.frontend/src/main/frontend/src/components/Login.js
+++ handlereg.web.frontend/src/main/frontend/src/components/Login.js
@@ -1,51 +1,49 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import { Redirect } from 'react-router';
 import login, { LOGIN_HENT } from '../reducers/login';
 const { USERNAME_ENDRE, PASSWORD_ENDRE } = login.actions;
 
-class Login extends Component {
-    render() {
-        const { username, password, loginresultat, onUsernameEndre, onPasswordEndre, onSendLogin } = this.props;
-        if (loginresultat.suksess) {
-            return (<Redirect to="/handlereg/" />);
-        }
-
-        return (
-            <div className="Login">
-                <header>
-                    <div className="pb-2 mt-4 mb-2 border-bottom bg-light">
-                        <h1>Handleregistrering login</h1>
-                        <p id="messagebanner"></p>
-                    </div>
-                </header>
-                <div className="container">
-                    <form onSubmit={e => { e.preventDefault(); }}>
-                        <div className="form-group row">
-                            <label htmlFor="username" className="col-form-label col-3 mr-2">Username:</label>
-                            <div className="col-8">
-                                <input id="username" className="form-control" type="text" name="username" value={username} onChange={e => onUsernameEndre(e.target.value)} />
-                            </div>
-                        </div>
-                        <div className="form-group row">
-                            <label htmlFor="password" className="col-form-label col-3 mr-2">Password:</label>
-                            <div className="col-8">
-                                <input id="password" className="form-control" type="password" name="password" value={password} onChange={e => onPasswordEndre(e.target.value)}/>
-                            </div>
-                        </div>
-                        <div className="form-group row">
-                            <div className="offset-xs-3 col-xs-9">
-                                <input className="btn btn-primary" type="submit" value="Login" onClick={() => onSendLogin(username, password)}/>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        );
+function Login(props) {
+    const { username, password, loginresultat, onUsernameEndre, onPasswordEndre, onSendLogin } = this.props;
+    if (loginresultat.suksess) {
+        return (<Redirect to="/handlereg/" />);
     }
+
+    return (
+        <div className="Login">
+            <header>
+                <div className="pb-2 mt-4 mb-2 border-bottom bg-light">
+                    <h1>Handleregistrering login</h1>
+                    <p id="messagebanner"></p>
+                </div>
+            </header>
+            <div className="container">
+                <form onSubmit={e => { e.preventDefault(); }}>
+                    <div className="form-group row">
+                        <label htmlFor="username" className="col-form-label col-3 mr-2">Username:</label>
+                        <div className="col-8">
+                            <input id="username" className="form-control" type="text" name="username" value={username} onChange={e => onUsernameEndre(e.target.value)} />
+                        </div>
+                    </div>
+                    <div className="form-group row">
+                        <label htmlFor="password" className="col-form-label col-3 mr-2">Password:</label>
+                        <div className="col-8">
+                            <input id="password" className="form-control" type="password" name="password" value={password} onChange={e => onPasswordEndre(e.target.value)}/>
+                        </div>
+                    </div>
+                    <div className="form-group row">
+                        <div className="offset-xs-3 col-xs-9">
+                            <input className="btn btn-primary" type="submit" value="Login" onClick={() => onSendLogin(username, password)}/>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
 }
 
-const mapStateToProps = state => {
+function mapStateToProps(state) {
     const login = state.login || {};
     const { username, password, loginresultat } = login;
     return {
@@ -53,15 +51,14 @@ const mapStateToProps = state => {
         password,
         loginresultat,
     };
-};
+}
 
-const mapDispatchToProps = dispatch => {
+function mapDispatchToProps(dispatch) {
     return {
         onUsernameEndre: (username) => dispatch(USERNAME_ENDRE(username)),
         onPasswordEndre: (password) => dispatch(PASSWORD_ENDRE(password)),
         onSendLogin: (username, password) => dispatch(LOGIN_HENT({ username, password })),
     };
-};
+}
 
-Login = connect(mapStateToProps, mapDispatchToProps)(Login);
-export default Login;
+export default connect(mapStateToProps, mapDispatchToProps)(Login);
--- handlereg.web.frontend/src/main/frontend/src/components/NyButikk.js
+++ handlereg.web.frontend/src/main/frontend/src/components/NyButikk.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { Header } from './bootstrap/Header';
@@ -7,50 +7,47 @@ import { StyledLinkLeft } from './bootstrap/StyledLinkLeft';
 import butikk, { NYBUTIKK_REGISTRER } from '../reducers/butikk';
 const { BUTIKKNAVN_ENDRE } = butikk.actions;
 
-class NyButikk extends Component {
-    render() {
-        const { butikk, endreNavn, onRegistrerNyButikk } = this.props;
+function NyButikk(props) {
+    const { butikk, endreNavn, onRegistrerNyButikk } = props;
 
-        return (
-            <div>
-                <StyledLinkLeft to="/handlereg">Opp til matregnskap</StyledLinkLeft>
-                <Header>
-                    <h1>Ny butikk</h1>
-                </Header>
-                <Container>
-                    <form onSubmit={ e => { e.preventDefault(); }}>
-                        <div className="form-group row">
-                            <label htmlFor="amount" className="col-form-label col-5">Ny butikk</label>
-                            <div className="col-7">
-                                <input id="amount" className="form-control" type="text" value={butikk.butikknavn} onChange={e => endreNavn(e.target.value)} />
-                            </div>
+    return (
+        <div>
+            <StyledLinkLeft to="/handlereg">Opp til matregnskap</StyledLinkLeft>
+            <Header>
+                <h1>Ny butikk</h1>
+            </Header>
+            <Container>
+                <form onSubmit={ e => { e.preventDefault(); }}>
+                    <div className="form-group row">
+                        <label htmlFor="amount" className="col-form-label col-5">Ny butikk</label>
+                        <div className="col-7">
+                            <input id="amount" className="form-control" type="text" value={butikk.butikknavn} onChange={e => endreNavn(e.target.value)} />
                         </div>
-                        <div className="form-group row">
-                            <div className="col-5"/>
-                            <div className="col-7">
-                                <button className="btn btn-primary" onClick={() => onRegistrerNyButikk(butikk)}>Legg til butikk</button>
-                            </div>
+                    </div>
+                    <div className="form-group row">
+                        <div className="col-5"/>
+                        <div className="col-7">
+                            <button className="btn btn-primary" onClick={() => onRegistrerNyButikk(butikk)}>Legg til butikk</button>
                         </div>
-                    </form>
-                </Container>
-            </div>
-        );
-    }
+                    </div>
+                </form>
+            </Container>
+        </div>
+    );
 }
 
-const mapStateToProps = state => {
+function mapStateToProps(state) {
     const { butikk } = state;
     return {
         butikk,
     };
-};
+}
 
-const mapDispatchToProps = dispatch => {
+function mapDispatchToProps(dispatch) {
     return {
         endreNavn: (butikknavn) => dispatch(BUTIKKNAVN_ENDRE(butikknavn)),
         onRegistrerNyButikk: (nybutikk) => dispatch(NYBUTIKK_REGISTRER(nybutikk)),
     };
-};
+}
 
-NyButikk = connect(mapStateToProps, mapDispatchToProps)(NyButikk);
-export default NyButikk;
+export default connect(mapStateToProps, mapDispatchToProps)(NyButikk);
